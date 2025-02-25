926b0019bea48da2df84352e3a063605eb2ece02
Renan Loes <renanloes8@gmail.com>
2025-02-24 23:16:11 -0300

    finalizacao materialDAO e model material

--- src/DAO/MaterialDAO.java
+++ src/DAO/MaterialDAO.java
@@ -1,6 +1,62 @@
 package DAO;
 
+import DAO.ConexaoBanco;
+import Model.Material;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaterialDAO {
+     
+    //Excluir Material por ID
+    public void excluirMaterial (int id) throws SQLException{
+        String query = "DELETE FROM materiais WHERE id = ?";
+        
+        try(Connection conexao = ConexaoBanco.conectar();
+            PreparedStatement ps = conexao.prepareStatement(query)){
+            
+             ps.setInt(1, id);
+             ps.executeUpdate();
+        }
+    }
     
+    //Cadastro Material
+    public void cadastrarMaterial(Material material) throws SQLException {
+        String query = "INSERT INTO materiais (id, categoria, quantidade, valor, descricao) VALUES (?,?,?,?)";
+        
+        try(Connection conexao = ConexaoBanco.conectar();
+            PreparedStatement ps = conexao.prepareStatement(query)){
+            
+            ps.setString(1, material.getId());
+            ps.setString(2, material.getCategoria);
+            ps.setInt(3, material.getQuantidade());
+            ps.setDouble(4, material.getValor());
+            ps.setString(5, material.getDescricao());
+        }    
+    }
+    
+    public List<Material> listarMaterial(String id, String categoria, int quantidade, String descricao) throws SQLException{
+        List<Material> materiais = new ArrayList<>();
+        String query = "SELECT * FROM materiais WHERE id LIKE ? AND categoria LIKE ? AND quantidade LIKE ? AND descricao LIKE ?";
+        
+        try(Connection conexao = ConexaoBanco.conectar();
+            PreparedStatement ps = conexao.prepareStatement(query)){
+            
+            ps.setString(1, "%" + id + "%");
+            ps.setString(2, "%" + categoria + "%");
+            ps.setString(3, "%" + quantidade + "%");
+            ps.setString(4, "%" + descricao + "%");
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Material material = new Material (id, categoria, quantidade, descricao);
+                material.setId(rs.getString("id"));
+                material.setCategoria(rs.getString("categoria"));
+                material.setQuantidade(rs.getInt("quantidade"));
+                material.setDescricao(rs.getString("descricao"));
+            }
+        }
+        
+        return null;
+    }
 }
--- src/Model/Material.java
+++ src/Model/Material.java
@@ -1,10 +1,59 @@
-
 package Model;
 
-/**
- *
- * @author Renan
- */
 public class Material {
+
+    
+
+    public Material(String id, String categoria, int quantidade, String descricao){
+    }   
+       
+    private String id;
+    private String categoria;
+    private int quantidade;
+    private int valor;
+    private String descricao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategoria;
+    
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+        
     
 }
